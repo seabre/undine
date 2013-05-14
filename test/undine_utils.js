c83c6a7e02fe940f72b25a2667fdad071f242a27
Sean Brewer <seabre986@gmail.com>
2013-05-14 01:12:19 -0400

    Add configuration option. Add more tests.

--- test/undine_utils.js
+++ test/undine_utils.js
@@ -12,4 +12,28 @@ describe('Undine Utils', function () {
       }
     });
   });
+
+  describe('#undineDir()', function () {
+    it('should return the undine application directory', function () {
+      assert.equal(utils.undineDir(), utils.getUserHome() + "/.undine");
+    });
+  });
+
+  describe('#configData()', function () {
+    it('should be a hash with account and token keys', function () {
+      var account = "test";
+      var token = "test";
+      var data = utils.configData(account, token);
+      assert.equal(true, "account" in data);
+      assert.equal(true, "token" in data);
+    });
+
+    it('should strip whitespace off of the config options', function () {
+      var account = "  test   ";
+      var token = "   test      ";
+      var data = utils.configData(account, token);
+      assert.equal("test", data.account);
+      assert.equal("test", data.token);
+    });
+  });
 });
